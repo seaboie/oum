2f06ee28f52ad0843b5706dfe45379db3c03b9b3
kritbovorn <kritbovorn@gmail.com>
2024-03-09 21:33:28 +0700

    add <Link> for go to sim-report

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
         // <div className="text-6xl font-bold underline">Hello, สวัสดีชาวโลก</div>
 
-        <div>
-            sim report
-        </div>
+        <main className="flex flex-col h-screen items-center justify-center">
+            <h1>ไปหน้า Sim Report</h1>
+            <Link
+                href='/sim-report'
+            >
+                <button className="px-4 py-2 bg-blue-600 hover:bg-blue-400 rounded-md mt-4 text-white">Open</button>
+            </Link>
+        </main>
     )
 }
